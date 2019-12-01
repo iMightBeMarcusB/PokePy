007988d678e6b6b416c273e1fc8baf4072df4efd
Marcus Bravim (?) <mvba2003@live.com>
2019-12-01 19:28:51 -0300

    Made bag state

--- PokePy.py
+++ PokePy.py
@@ -1,4 +1,5 @@
 import pygame
+import Coordinates as cord
 
 ######### COLORS #########
 
@@ -14,29 +15,6 @@ BLACK =    (0,     0,   0  )
 ALPHA =    (255,   0,   255)
 GRAY =     (72,   72,   72 )
 
-########### BUTTONS AND ARROWS ###########
-
-#POSIÇÃO DAS OPÇÕES DE BATALHA
-fight_a = (425, 468)
-pokemon_a = (425, 528)
-bag_a = (611, 468)
-run_a = (611, 528)
-upper_a = 468
-bottom_a = 528
-left_a = 425
-right_a = 611
-
-#POSIÇÃO DOS BOTÕES DE ATAQUE
-left_attack_b = 50
-right_attack_b = 300
-upper_attack_b = 460
-bottom_attack_b = 520
-
-#POSIÇÕES DA SETA DE ATAQUE
-left_attack_a = left_attack_b - 25
-right_attack_a = right_attack_b - 25
-upper_attack_a = upper_attack_b - 5
-bottom_attack_a = bottom_attack_b - 5
 
 ######### FONTS #########
 
@@ -57,9 +35,11 @@ battle_arena = img('./InterfaceSprites/fgt_background.png')
 txt_bar = img('./InterfaceSprites/text_bar.png')
 fgt_options = img('./InterfaceSprites/fgt_options.png')
 arrow = img('./InterfaceSprites/arrow.png')
+arrow2 = img('./InterfaceSprites/Arrow2.0.png')
 atk_bar = img('./InterfaceSprites/pp_bar.png')
 title = img('./InterfaceSprites/PokePy.png')
 python = img('./InterfaceSprites/python_logo.png')
+bag_menu = img('./InterfaceSprites/Backpack_menu.png')
 
 ########### SCALE ADJUSTMENTS ###########
 
@@ -67,9 +47,11 @@ battle_arena = scale(battle_arena, (800, 420))
 txt_bar = scale(txt_bar, (800, 180))
 fgt_options = scale(fgt_options, (400, 180))
 arrow = scale(arrow, (20, 38))
+arrow2 = scale(arrow2, (33, 33))
 atk_bar = scale(atk_bar, (800, 180))
 title = scale(title, (500, 221))
 python = scale(python, (200, 200))
+bag_menu = scale(bag_menu, (800, 600))
 
 ########### ARROW ###########
 
@@ -79,8 +61,9 @@ class Arrow:
         self.y = y
         self.position = (self.x, self.y)
 
-battle_arrow = Arrow(left_a, upper_a)
-attack_arrow = Arrow(left_attack_a, upper_attack_a)
+battle_arrow = Arrow(cord.left_a, cord.upper_a)
+attack_arrow = Arrow(cord.left_attack_a, cord.upper_attack_a)
+bag_arrow = Arrow(380, 78)
 
 ########### GAMEPLAY ###########
 
@@ -181,7 +164,7 @@ class GameState:
             text_rect = text_object.get_rect(center=(x, y))
             surface.blit(text_object, text_rect)
 
-        if insertion == 'lefttop':
+        if insertion == 'topleft':
             text_rect = text_object.get_rect()
             text_rect.topleft = (x, y)
             surface.blit(text_object, text_rect)
@@ -194,6 +177,7 @@ class SplashScreen(GameState):
     def __init__(self):
         super().__init__()
         self.next_state = "BATTLE OPTIONS"
+
 
     def get_event(self, event):
         if event.type == pygame.QUIT:
@@ -224,16 +208,12 @@ class PokemonSelection(GameState):
 
 
 class BattleOptions(GameState):
-
     def __init__(self):
         super().__init__()
 
-    def draw(self, surface):
-        surface.blit(battle_arena, (0, 0))
-        surface.blit(txt_bar, (0, 420))
-        surface.blit(fgt_options, (400, 420))
+    def startup(self, persistent):
+        pass
 
-        surface.blit(arrow, (battle_arrow.x, battle_arrow.y))
 
     def get_event(self, event):
 
@@ -243,18 +223,35 @@ class BattleOptions(GameState):
 
         pressed = pygame.key.get_pressed()
 
-        if pressed[pygame.K_RIGHT] and battle_arrow.x == left_a:
-            battle_arrow.x += 186
-        if pressed[pygame.K_LEFT] and battle_arrow.x == right_a:
-            battle_arrow.x -= 186
-        if pressed[pygame.K_UP] and battle_arrow.y == bottom_a:
-            battle_arrow.y -= 60
-        if pressed[pygame.K_DOWN] and battle_arrow.y == upper_a:
-            battle_arrow.y += 60
+        if pressed[pygame.K_RIGHT] and battle_arrow.x == cord.left_a:
+            battle_arrow.x -= cord.left_a - cord.right_a
+        if pressed[pygame.K_LEFT] and battle_arrow.x == cord.right_a:
+            battle_arrow.x += cord.left_a - cord.right_a
+        if pressed[pygame.K_UP] and battle_arrow.y == cord.bottom_a:
+            battle_arrow.y -= cord.bottom_a - cord.upper_a
+        if pressed[pygame.K_DOWN] and battle_arrow.y == cord.upper_a:
+            battle_arrow.y += cord.bottom_a - cord.upper_a
 
-        if pressed[pygame.K_RETURN] and (battle_arrow.x, battle_arrow.y) == fight_a:
+        if pressed[pygame.K_RETURN] and (battle_arrow.x, battle_arrow.y) == cord.fight_a:
             self.done = True
             self.next_state = 'ATTACK OPTIONS'
+            attack_arrow.x, attack_arrow.y = cord.left_attack_a, cord.upper_attack_a  # Faz a seta voltar para a posição inicial
+
+        if pressed[pygame.K_RETURN] and (battle_arrow.x, battle_arrow.y) == cord.bag_a:
+            self.done = True
+            self.next_state = "BACKPACK"
+
+    def update(self, dt):
+        pass
+
+
+    def draw(self, surface):
+        surface.blit(battle_arena, (0, 0))
+        surface.blit(txt_bar, (0, 420))
+        surface.blit(fgt_options, (400, 420))
+
+        surface.blit(arrow, (battle_arrow.x, battle_arrow.y))
+
 
     def persist(self):
         pass
@@ -269,15 +266,8 @@ class AttackOptions(GameState):
     def __init__(self):
         super().__init__()
 
-    def draw(self, surface):
-        surface.blit(battle_arena, (0, 0))
-        surface.blit(atk_bar, (0, 420))
-        surface.blit(arrow, (attack_arrow.x, attack_arrow.y))
-
-        self.write_text('lefttop', surface, monospace, 20, 'passa zap', left_attack_b, upper_attack_b, GRAY)
-        self.write_text('lefttop', surface, monospace, 20, 'quick attack', right_attack_b, upper_attack_b, GRAY)
-        self.write_text('lefttop', surface, monospace, 20, 'thunder wave', left_attack_b, bottom_attack_b, GRAY)
-        self.write_text('lefttop', surface, monospace, 20, 'bite', right_attack_b, bottom_attack_b, GRAY)
+    def startup(self, persistent):
+        pass
 
     def get_event(self, event):
 
@@ -287,21 +277,86 @@ class AttackOptions(GameState):
 
         pressed = pygame.key.get_pressed()
 
-        if pressed[pygame.K_RIGHT] and attack_arrow.x == left_attack_a:
-            attack_arrow.x -= left_attack_a - right_attack_a
-        if pressed[pygame.K_LEFT] and attack_arrow.x == right_attack_a:
-            attack_arrow.x += left_attack_a - right_attack_a
-        if pressed[pygame.K_UP] and attack_arrow.y == bottom_attack_a:
-            attack_arrow.y -= bottom_attack_a - upper_attack_a
-        if pressed[pygame.K_DOWN] and attack_arrow.y == upper_attack_a:
-            attack_arrow.y += bottom_attack_a - upper_attack_a
+        if pressed[pygame.K_RIGHT] and attack_arrow.x == cord.left_attack_a:
+            attack_arrow.x -= cord.left_attack_a - cord.right_attack_a
+        if pressed[pygame.K_LEFT] and attack_arrow.x == cord.right_attack_a:
+            attack_arrow.x += cord.left_attack_a - cord.right_attack_a
+        if pressed[pygame.K_UP] and attack_arrow.y == cord.bottom_attack_a:
+            attack_arrow.y -= cord.bottom_attack_a - cord.upper_attack_a
+        if pressed[pygame.K_DOWN] and attack_arrow.y == cord.upper_attack_a:
+            attack_arrow.y += cord.bottom_attack_a - cord.upper_attack_a
 
         if pressed[pygame.K_ESCAPE]:
             self.done = True
             self.next_state = 'BATTLE OPTIONS'
 
-        def update(self, dt):
-            pass
+    def update(self, dt):
+        pass
+
+
+    def draw(self, surface):
+        surface.blit(battle_arena, (0, 0))
+        surface.blit(atk_bar, (0, 420))
+        surface.blit(arrow, (attack_arrow.x, attack_arrow.y))
+
+        attack1 = 'cala'
+        attack2 = 'a boca'
+        attack3 = 'vadia'
+        attack4 = 'puta'
+
+        self.write_text('topleft', surface, monospace, 20, attack1, cord.left_attack_b, cord.upper_attack_b, GRAY)
+        self.write_text('topleft', surface, monospace, 20, attack2, cord.right_attack_b, cord.upper_attack_b, GRAY)
+        self.write_text('topleft', surface, monospace, 20, attack3, cord.left_attack_b, cord.bottom_attack_b, GRAY)
+        self.write_text('topleft', surface, monospace, 20, attack4, cord.right_attack_b, cord.bottom_attack_b, GRAY)
+
+
+
+class Backpack(GameState):
+    def __init__(self):
+        super().__init__()
+
+    def startup(self, persistent):
+        pass
+
+    def get_event(self, event):
+
+        if event.type == pygame.QUIT:
+            self.quit = True
+            self.done = True
+
+        pressed = pygame.key.get_pressed()
+
+        if pressed[pygame.K_UP] and not bag_arrow.y == cord.close_bag_a:
+            bag_arrow.y += cord.close_bag_a - cord.first_item_a
+        if pressed[pygame.K_DOWN] and not bag_arrow.y == cord.second_item_a:
+            bag_arrow.y -= cord.close_bag_a - cord.first_item_a
+
+        if pressed[pygame.K_ESCAPE]:
+            self.done = True
+            self.next_state = 'BATTLE OPTIONS'
+            bag_arrow.x = cord.left_bag_a
+            bag_arrow.y = cord.close_bag_a
+
+        if pressed[pygame.K_RETURN] and (bag_arrow.x, bag_arrow.y) == (cord.left_bag_a, cord.close_bag_a):
+            self.done = True
+            self.next_state = "BATTLE OPTIONS"
+
+    def update(self, dt):
+        pass
+
+    def draw(self, surface):
+        surface.blit(bag_menu, (0,0))
+        text1 = 'What would you like'
+        text2 = 'to do?'
+
+        self.write_text('topleft', surface , monospace, 20, text1 , 24, 410, BLACK)
+        self.write_text('topleft', surface , monospace, 20, text2 , 24, 440, BLACK)
+
+        self.write_text('topleft', surface, monospace, 30, 'CLOSE BAG', 420, 74, BLACK)
+        self.write_text('topleft', surface, monospace, 30, 'POTION', 420, 114, BLACK)
+        self.write_text('topleft', surface, monospace, 30, 'SUPERPOTION', 420, 154, BLACK)
+
+        surface.blit(arrow2, (bag_arrow.x, bag_arrow.y))
 
 
 
@@ -312,7 +367,8 @@ if __name__ == "__main__":
     screen = pygame.display.set_mode((800, 600))
     states = {"SPLASH": SplashScreen(),
               "BATTLE OPTIONS": BattleOptions(),
-              "ATTACK OPTIONS": AttackOptions()}
+              "ATTACK OPTIONS": AttackOptions(),
+              "BACKPACK": Backpack()}
     game = Game(screen, states, "SPLASH")
     game.run()
     pygame.quit()
