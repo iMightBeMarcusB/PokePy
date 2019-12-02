e2ea94b498a41ba3eca7edf0a75b68072cc4db3a
Marcus Bravim (?) <mvba2003@live.com>
2019-12-01 21:36:03 -0300

    Ye

---
+++ Coordinates.py
@@ -0,0 +1,46 @@
+#######################################
+'''
+    posição da seta durante o estágio de
+    opções de batalha
+'''
+fight_a = (425, 468)
+pokemon_a = (425, 528)
+bag_a = (611, 468)
+run_a = (611, 528)
+upper_a = 468
+bottom_a = 528
+left_a = 425
+right_a = 611
+
+#######################################
+''' 
+    posição do texto referente aos
+    ataques de cada pokemon, no
+    estágio de selção de ataque
+'''
+
+left_attack_b = 50
+right_attack_b = 300
+upper_attack_b = 460
+bottom_attack_b = 520
+
+#######################################
+'''
+    posição da seta durante o estágio 
+    de seleção de ataque
+'''
+
+left_attack_a = left_attack_b - 25
+right_attack_a = right_attack_b - 25
+upper_attack_a = upper_attack_b - 5
+bottom_attack_a = bottom_attack_b - 5
+
+#######################################
+'''
+   posição da seta durante o estágio
+    da mochila'''
+
+close_bag_a =(78)
+left_bag_a = (380)
+first_item_a = (118)
+second_item_a = (158)
